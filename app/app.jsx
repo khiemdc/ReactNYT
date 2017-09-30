7fb95591e4899f479ad14adc17389e418c4e699e
khiemdc <khiemle@verizon.net>
2017-09-30 19:49:33 -0400

    React New York Times Search

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+
+
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Parent Component
+var Main = require("./components/children/Main.jsx");
+
+// This code here allows us to render our main component (in this case Main)
+// the Id is "app" which matches that of the "index.html" file
+ReactDOM.render(<Main />, document.getElementById("app"));
